ccc64278ab94e5d04ef5a1ca229244ced3b1b9f7
Stacey <staceyg176@gmail.com>
2026-05-10 23:27:53 -0400

    nf test

--- federation.config.js
+++ federation.config.js
@@ -5,9 +5,9 @@ module.exports = withNativeFederation({
 
   exposes: {
     './Component': './src/app/app.ts',
-    //'./WeeklyTrend': './src/app/components/weeklytrend/weeklytrend.ts',
-    './LogMood': './src/app/components/logmood/logmood.ts',
-    //'./Trends': './src/app/components/weeklytrend/weeklytrend.ts',
+    './WeeklyTrend': './src/app/app.ts',
+    './LogMood': './src/app/logmoodpage/logmoodpage.ts',
+    './Trends': './src/app/components/weeklytrend/weeklytrend.ts',
     './Card': './src/app/components/weeklytrend/weeklytrend.ts'
   },
 
