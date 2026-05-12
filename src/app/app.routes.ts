a2752ceb9180078cab1f05edf4f228421e96122a
Stacey <staceyg176@gmail.com>
2026-05-11 22:48:03 -0400

    divider update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,23 +4,25 @@ import { Dashboard } from './dashboard/dashboard';
 import { Seetrends } from './seetrends/seetrends';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./dashboard/dashboard').then(m => m.Dashboard),
-    title: 'Dashboard'
-  },
-  {
-    path: 'logmoodpage',
-    loadComponent: () =>
-      import('./logmoodpage/logmoodpage').then(m => m.Logmoodpage),
-    title: 'Log Your Mood'
-  },
-  {
-    path: 'seetrends',
-    loadComponent: () =>
-      import('./seetrends/seetrends').then(m => m.Seetrends),
-    title: 'Trends'
-  },
+
+  
+  // {
+  //   path: '',
+  //   loadComponent: () =>
+  //     import('./dashboard/dashboard').then(m => m.Dashboard),
+  //   title: 'Dashboard'
+  // },
+  // {
+  //   path: 'logmoodpage',
+  //   loadComponent: () =>
+  //     import('./logmoodpage/logmoodpage').then(m => m.Logmoodpage),
+  //   title: 'Log Your Mood'
+  // },
+  // {
+  //   path: 'seetrends',
+  //   loadComponent: () =>
+  //     import('./seetrends/seetrends').then(m => m.Seetrends),
+  //   title: 'Trends'
+  // },
   { path: '**', redirectTo: '' }
 ];
